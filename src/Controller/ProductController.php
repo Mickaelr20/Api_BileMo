0ef20f232401659aaa755f47e63b50fc00f747da
Mickaelr20 <85870744+Mickaelr20@users.noreply.github.com>
2023-01-09 16:33:44 +0400

    Route listing produits simple

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+// #[Route('/products', name: 'app_products_')]
+class ProductController extends AbstractController
+{
+
+    #[Route('/products/list', name: 'list', methods: ['GET'])]
+    public function list(Request $request, ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->listPage($request->query->getInt("page", 1), 1);
+        // $products = ["test" => "test"];
+        return $this->json($products);
+    }
+}
