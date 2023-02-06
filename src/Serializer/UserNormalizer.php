6fc4074d1dae40fc54c95cb30d9201e981e57228
Mickaelr20 <85870744+Mickaelr20@users.noreply.github.com>
2023-02-06 16:55:40 +0400

    Création UserNormalizer
    Ajout self et delete pour le niveau 3 de maturité de Richarson

---
+++ src/Serializer/UserNormalizer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Serializer;
+
+use App\Entity\User;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+class UserNormalizer implements NormalizerInterface
+{
+    private $router;
+    private $normalizer;
+
+    public function __construct(UrlGeneratorInterface $router, ObjectNormalizer $normalizer)
+    {
+        $this->router = $router;
+        $this->normalizer = $normalizer;
+    }
+
+    public function normalize($user, string $format = null, array $context = [])
+    {
+        $data = $this->normalizer->normalize($user, $format, $context);
+
+        $data['_links']['self'] = $this->router->generate('api_users_view', [
+            'id' => $user->getId(),
+        ], UrlGeneratorInterface::ABSOLUTE_URL);
+        $data['_links']['delete'] = $this->router->generate('api_users_delete', [
+            'id' => $user->getId(),
+        ], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return $data;
+    }
+
+    public function supportsNormalization($data, string $format = null, array $context = [])
+    {
+        return $data instanceof User;
+    }
+}
