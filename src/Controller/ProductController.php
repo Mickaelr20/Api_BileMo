51744e749d3602b678670a3e1352877ec6313709
Mickaelr20 <85870744+Mickaelr20@users.noreply.github.com>
2023-03-13 18:51:22 +0400

    Refactor : suppression import inutilisé

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,8 +9,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Nelmio\ApiDocBundle\Annotation\Model;
-use OpenApi\Attributes as OA;
 
 #[Route('/products', name: 'api_products_')]
 class ProductController extends AbstractController
