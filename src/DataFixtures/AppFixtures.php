989977713c5c258a87c7e78808fcc78ef217606e
Mickaelr20 <85870744+Mickaelr20@users.noreply.github.com>
2023-03-26 20:53:30 +0400

    Fixtures : Ajout de données pour examples

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Brand;
 use App\Entity\Client;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Product;
@@ -23,15 +24,15 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 10; ++$i) {
+        for ($i = 0; $i < 25; ++$i) {
             $brand = new Brand();
-            $brand->setCode('BR'.$i);
+            $brand->setCode('BR' . $i);
             $name = $this->faker->word();
             $brand->setName($name);
             $manager->persist($brand);
 
             $product = new Product();
-            $product->setReference('P-'.str_pad($i, 5, '0', STR_PAD_LEFT));
+            $product->setReference('P-' . str_pad($i, 5, '0', STR_PAD_LEFT));
             $product->setName($this->faker->words(3, true));
             $product->setBrand($brand);
             $product->setDescription($this->faker->sentence(15));
@@ -43,6 +44,32 @@ class AppFixtures extends Fixture
         $client->setEmail(self::DEFAULT_USER_EMAIL);
         $client->setPassword($this->passwordHasher->hashPassword($client, self::DEFAULT_USER_PASSWORD));
         $manager->persist($client);
+
+        for ($i = 0; $i < 3; $i++) {
+            $randomUser = new User();
+            $randomUser->setFirstName($this->faker->firstName());
+            $randomUser->setLastName($this->faker->LastName());
+            $randomUser->setEmail($this->faker->Email());
+            $randomUser->setClient($client);
+            $manager->persist($randomUser);
+        }
+
+        for ($i = 0; $i < 3; $i++) {
+            $randomClient = new Client();
+            $randomClient->setEmail($this->faker->email());
+            $randomClient->setPassword($this->passwordHasher->hashPassword($randomClient, $this->faker->password()));
+            $manager->persist($randomClient);
+
+            for ($i = 0; $i < 3; $i++) {
+                $randomUser = new User();
+                $randomUser->setFirstName($this->faker->firstName());
+                $randomUser->setLastName($this->faker->LastName());
+                $randomUser->setEmail($this->faker->Email());
+                $randomUser->setClient($randomClient);
+                $manager->persist($randomUser);
+            }
+        }
+
 
         $manager->flush();
     }
